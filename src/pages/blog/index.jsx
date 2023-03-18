4b81a15e3de0c8b9751e67009d87559447fd9f25
Darlley <systembrtt@gmail.com>
2023-03-17 21:03:51 -0400

    update

--- src/pages/blog/index.jsx
+++ src/pages/blog/index.jsx
@@ -1,6 +1,7 @@
 import HeaderPage from "@/components/molecules/HeaderPage";
 import Metadata from "@/components/molecules/Metadata";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
 const metadata = {
   title: "Darlley - Blog",
@@ -8,23 +9,40 @@ const metadata = {
   image: "/lotr-1440x522.png"
 }
 
-export async function getStaticProps(){
-  const data = await fetch('https://www.darlley.dev/api/notion')
-  const articles = await data.json()
-  
-  return {
-    props: {
-      articles
+const URL_API = 'https://darlley.dev/api/notion'
+
+function Blog () {
+  const [loading, setLoading] = useState(false)
+  const [articles, setArticles] = useState([])
+
+  const fetchArticles = async () => {
+    try {
+      setLoading(true)
+
+      const res = await fetch(URL_API)
+      const data = await res.json()
+
+      if(!data) throw "Missing data..."
+
+      setArticles(data)
+
+    } catch(error){
+      console.log(error)
+    } finally {
+      setLoading(false)
     }
   }
-}
 
-function Blog ({articles}) {
+  useEffect(() => {
+    fetchArticles()
+  }, [])
 
   const frases = [
     'O problema do mundo de hoje é que as pessoas inteligentes estão cheias de dúvidas, e as pessoas idiotas estão cheias de certezas. — Bertrand Russell',
     'O que eu conheço é uma gota, o que ignoro é um oceano. — Sr. Isaac Newton'
   ]
+
+  console.log(loading, articles)
 
   return (
     <>
@@ -47,36 +65,34 @@ function Blog ({articles}) {
         </div>
 
         <div className="articles__container">
-          <ul className="articles">
-            {articles.map((article) => 
-              <li className="article" key={article.id}>
-                <div className="flex flex-col w-full ">
-                  <span className="font-mono text-xs text-slate-500">{article.date}</span>
-                  <div className="flex items-start justify-between w-full">
-                    <h3 className="w-full">
-                      <Link href={`/blog/${article.slug}`} className="flex py-2 text-lg font-bold text-slate-800 hover:text-primary-500">
-                        {article.title}
-                      </Link>
-                    </h3>
-                    {article.thumbnail && 
-                    <div className="flex flex-col items-end justify-end gap-4 w-max">
-                      <div className="w-[60px] h-[60px]">
-                        <img src={article.thumbnail} alt="thumbnail" />
-                      </div>
-                      <span className="font-mono text-xs text-right text-slate-500 w-max"><a href={article.url} target="_blank">Read in Notion</a></span>
-                    </div>
-                    }
-                  </div>
-                  
-                </div>
-                
-              </li>
-            )}
-        </ul>
-          {articles.length <= 0 &&
+          {loading || articles.length <= 0 ? 
             <div className="w-full text-center">
               <h4 className="text-lg text-slate-500">Artigos em breve 🔥</h4>
-            </div>
+            </div> : 
+            <ul className="articles">
+              {articles.map((article) => (
+                <li className="article" key={article.id}>
+                  <div className="flex flex-col w-full ">
+                    <span className="font-mono text-xs text-slate-500">{article.date}</span>
+                    <div className="flex items-start justify-between w-full">
+                      <h3 className="w-full">
+                        <Link href={`/blog/${article.slug}`} className="flex py-2 text-lg font-bold text-slate-800 hover:text-primary-500">
+                          {article.title}
+                        </Link>
+                      </h3>
+                      {article.thumbnail && 
+                      <div className="flex flex-col items-end justify-end gap-4 w-max">
+                        <div className="w-[60px] h-[60px]">
+                          <img src={article.thumbnail} alt="thumbnail" />
+                        </div>
+                        <span className="font-mono text-xs text-right text-slate-500 w-max"><a href={article.url} target="_blank">Read in Notion</a></span>
+                      </div>
+                      }
+                    </div>
+                  </div>
+                </li>)
+              )}
+            </ul>
           }
         </div>
       </main>
