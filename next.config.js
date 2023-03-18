6daff66873d64f207208118a70910f9e8f3a3f26
Darlley <systembrtt@gmail.com>
2023-03-18 15:48:56 -0400

    install nextjs-cors

--- next.config.js
+++ next.config.js
@@ -4,16 +4,11 @@ const nextConfig = {
 }
 
 module.exports = {
-  async headers() {
+  async rewrites() {
     return [
       {
-        source: '/:path*',
-        headers: [
-          { key: 'Access-Control-Allow-Credentials', value: 'true' },
-          { key: 'Access-Control-Allow-Origin', value: '*' },
-          { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' },
-          { key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization' },
-        ],
+        source: '/api/:path*',
+        destination: 'https://www.darlley.dev/:path*'
       },
     ];
   },
