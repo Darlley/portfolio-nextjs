1857bb2e257be1a892449fe0490563b4fb2a74af
Darlley <systembrtt@gmail.com>
2023-03-17 22:19:20 -0400

    update

--- next.config.js
+++ next.config.js
@@ -9,6 +9,12 @@ module.exports = {
       {
         source: '/api/:path*',
         destination: 'https://www.darlley.dev/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET' },
+          { key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, Accept, Accep-Version, Content-Length, Content-MD5, Content-Type, Date, X-API-Version, Authorization' },
+        ]
       },
     ]
   },
