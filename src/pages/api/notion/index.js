b74cfb836c432d6f589061772a96d0a4734d23be
Darlley Brito <darlleybrito@gmail.com>
2023-03-20 12:12:38 -0300

    remove nextjs-cors

--- src/pages/api/notion/index.js
+++ src/pages/api/notion/index.js
@@ -1,5 +1,4 @@
 import { Client } from "@notionhq/client"
-import NextCors from "nextjs-cors"
 
 const notion_secret = process.env.NOTION_TOKEN
 const notion_database = process.env.NOTION_DATABASE_ID
@@ -8,12 +7,6 @@ const notion_database = process.env.NOTION_DATABASE_ID
 const notion = new Client({ auth: notion_secret })
 
 export default async function handler(req, res) {
-  await NextCors(req, res, {
-    // Options
-    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
-    origin: '*',
-    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
-  });
 
   if(!notion_secret || !notion_database) throw new Error('Sem Token do Notion ou ID do Banco de Dados.')
 
