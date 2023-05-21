85453fa94d39c50f78f682a1567df25ec941c66b
Darlley <systembrtt@gmail.com>
2023-05-20 20:41:35 -0400

    add typescript and editorconfig

---
+++ src/components/atoms/doc.md
@@ -0,0 +1,11 @@
+# ATOMIC DESIGN (ATOMS)
+
+Componentes:
+
+- botões
+- Titulos
+- Inputs
+- Cores
+- Animações
+- Fontes
+- Icones SVG
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import '@/styles/globals.css'
 import '@/styles/nprogress.css'
+import { NextPage } from 'next';
 import {Router} from 'next/router'
 import NProgress from 'nprogress'
 
@@ -7,6 +8,10 @@ Router.events.on('routeChangeStart', (url) => NProgress.start());
 Router.events.on('routeChangeComplete', () => NProgress.done());
 Router.events.on('routeChangeError', () => NProgress.done());
 
-export default function App({ Component, pageProps }) {
+type AppProps = {
+  Component: NextPage<any>;
+  pageProps: any;
+}
+export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
