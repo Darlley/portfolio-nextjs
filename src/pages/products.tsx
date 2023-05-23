3e11e30d3e2375ec0e0ab2fc8df5d271045c9171
Darlley <systembrtt@gmail.com>
2023-05-22 20:30:36 -0400

    update categories

--- src/pages/products.tsx
+++ src/pages/products.tsx
@@ -22,14 +22,64 @@ export default function ProductsPage() {
       <HeaderPage>
         <h1>SHOPPING</h1>
       </HeaderPage>
-      <main className="py-8 px-4 2xl:px-0 max-w-7xl m-auto w-full flex flex-col gap-4">
+      <main className="py-8 px-4 2xl:px-0 max-w-7xl m-auto w-full flex flex-col gap-4 relative">
         <div className="">
           <h3 className="text-xl font-bold text-primary-400">Produtos</h3>
           <p>Produtos que uso no meu setup, livros, limpeza automotiva, roupas...</p>
         </div>
 
-        <div className="w-full">
-          <div className="opacity-80 hover:opacity-100 group h-full max-w-[300px] rounded-md overflow-hidden border hover:shadow-lg">
+        <div className="py-4 w-full sticky top-0 bg-white bg-opacity-95 z-10">
+          <ul className="flex gap-2 flex-wrap w-full">
+            <li>
+              <button className="transition-colors flex items-center gap-1 bg-secundary-900 rounded-full text-xs p-1 hover:bg-opacity-100 text-white">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-3 h-3">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M9.568 3H5.25A2.25 2.25 0 003 5.25v4.318c0 .597.237 1.17.659 1.591l9.581 9.581c.699.699 1.78.872 2.607.33a18.095 18.095 0 005.223-5.223c.542-.827.369-1.908-.33-2.607L11.16 3.66A2.25 2.25 0 009.568 3z" />
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M6 6h.008v.008H6V6z" />
+                </svg>
+                Todos
+              </button>
+            </li>
+            <li>
+              <button className="transition-colors flex items-center gap-1 bg-secundary-500 rounded-full text-xs text-secundary-900 p-1 hover:bg-opacity-100 bg-opacity-80 hover:text-white">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-3 h-3">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M9.568 3H5.25A2.25 2.25 0 003 5.25v4.318c0 .597.237 1.17.659 1.591l9.581 9.581c.699.699 1.78.872 2.607.33a18.095 18.095 0 005.223-5.223c.542-.827.369-1.908-.33-2.607L11.16 3.66A2.25 2.25 0 009.568 3z" />
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M6 6h.008v.008H6V6z" />
+                </svg>
+                Setup
+              </button>
+            </li>
+            <li>
+              <button className="transition-colors flex items-center gap-1 bg-secundary-500 rounded-full text-xs text-secundary-900 p-1 hover:bg-opacity-100 bg-opacity-80 hover:text-white">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-3 h-3">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M9.568 3H5.25A2.25 2.25 0 003 5.25v4.318c0 .597.237 1.17.659 1.591l9.581 9.581c.699.699 1.78.872 2.607.33a18.095 18.095 0 005.223-5.223c.542-.827.369-1.908-.33-2.607L11.16 3.66A2.25 2.25 0 009.568 3z" />
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M6 6h.008v.008H6V6z" />
+                </svg>
+                Livros
+              </button>
+            </li>
+            <li>
+              <button className="transition-colors flex items-center gap-1 bg-secundary-500 rounded-full text-xs text-secundary-900 p-1 hover:bg-opacity-100 bg-opacity-80 hover:text-white">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-3 h-3">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M9.568 3H5.25A2.25 2.25 0 003 5.25v4.318c0 .597.237 1.17.659 1.591l9.581 9.581c.699.699 1.78.872 2.607.33a18.095 18.095 0 005.223-5.223c.542-.827.369-1.908-.33-2.607L11.16 3.66A2.25 2.25 0 009.568 3z" />
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M6 6h.008v.008H6V6z" />
+                </svg>
+                Limpeza Automotiva
+              </button>
+            </li>
+            <li>
+              <button className="transition-colors flex items-center gap-1 bg-secundary-500 rounded-full text-xs text-secundary-900 p-1 hover:bg-opacity-100 bg-opacity-80 hover:text-white">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-3 h-3">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M9.568 3H5.25A2.25 2.25 0 003 5.25v4.318c0 .597.237 1.17.659 1.591l9.581 9.581c.699.699 1.78.872 2.607.33a18.095 18.095 0 005.223-5.223c.542-.827.369-1.908-.33-2.607L11.16 3.66A2.25 2.25 0 009.568 3z" />
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M6 6h.008v.008H6V6z" />
+                </svg>
+                Roupa
+              </button>
+            </li>
+          </ul>
+        </div>
+
+        <div className="w-full flex flex-wrap gap-2 justify-around">
+          <div className="md:opacity-80 md:hover:opacity-100 group h-full max-w-[220px] rounded-md overflow-hidden border hover:shadow-lg">
             <div className="relative w-full">
               <div className="h-52 overflow-hidden flex-col flex items-center">
                 <Image src="/lpro40.png" width={640} height={640} alt="fones" />
@@ -43,11 +93,11 @@ export default function ProductsPage() {
             <div className="flex items-center justify-between md:items-start p-4">
               <div className="flex flex-col">
                 <p className="text-lg font-bold text-secundary-500">Lenovo LP40 Pro</p>
-                <p className="text-base font-normal text-primary-800">Fones de ouvido sem fio Bluetooth 5.1</p>
+                <p className="text-md font-normal text-primary-800">Fones de ouvido sem fio Bluetooth 5.1</p>
               </div>
             </div>
             <div className="flex p-4">
-              <a href="https://shope.ee/30IohUt5m5" target="_blank" className="group-hover:text-white transition-all duration-200 bg-opacity-80 bg-primary-500 hover:bg-opacity-100 text-white rounded-md w-full text-center p-2">Ir para Shopee</a>
+              <a href="https://shope.ee/30IohUt5m5" target="_blank" className="transition-all duration-200 bg-opacity-80 bg-primary-500 hover:bg-opacity-100 text-white rounded-md w-full text-center p-2">Ir para Shopee</a>
             </div>
           </div>
         </div>
